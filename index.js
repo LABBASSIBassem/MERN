f3b9d521fffc8e5f3952e53ddedf1fa40eb2dd52
unknown <bassem.labbassi@gmail.com>
2020-03-30 01:08:48 +0100

    second_commit

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const mongoose = require('mongoose'); 
-require('dotenv/config')
 
+const bodyParser = require('body-parser'); 
+const cookieParser = require('cookie-parser')
 const app = express();
 
-mongoose.connect(process.env.MONGO,
-{ useNewUrlParser: true, useUnifiedTopology: true},
-(err)=>{
-    if(err) throw err; 
-    console.log("successfully connected to the database")
+const config = require('./config/key')
+const User = require('./Models/user')
+
+mongoose.connect(config.mongoURI,
+    { useNewUrlParser: true, useUnifiedTopology: true},
+    (err)=>{
+        if(err) throw err; 
+        console.log("successfully connected to the database")
+    })
+
+
+app.use(cookieParser())
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/api/users/register', (req, res) =>{
+  
+     const user = new User(req.body);
+     user.save((err, userData)=>{
+         if(err)  return res.json({success: false, err})
+         res.send("jawkbehi")
+     })
+   
+
+
 })
+
+
+
+
+
 PORT= process.env.PORT || 5000; 
-
-app.get('/', (req, res)=>{
-    res.send("hello world")
-})
-
 
 app.listen(PORT, ()=>{
     console.log("server is running")
